8bd1d9a3ee077441ed8ff742bbab28f89a3c0ff4
skulter <skulter@naver.com>
2022-03-31 10:32:22 +0900

    다이나믹프로그래밍_문제풀어야함

--- 파이썬_코딩테스트/이진탐색/특정범위에속하는데이터개수구하기.py
+++ 파이썬_코딩테스트/이진탐색/특정범위에속하는데이터개수구하기.py
@@ -1,7 +1,5 @@
 from bisect import bisect_left, bisect_right
-
 # 값이 [left_value, right_value]인 데이터의 개수를 반환하는 함수
-
 
 def count_by_range(array, left_value, right_value):
     right_index = bisect_right(array, right_value)
@@ -9,7 +7,12 @@ def count_by_range(array, left_value, right_value):
     return right_index-left_index
 
 
-array = [1, 2, 3, 3, 3, 3, 4, 4, 8, 9]
+n, x = map(int, input().split())
+array = list(map(int, input().split()))
 
-print(count_by_range(array, 4, 4))
-print(count_by_range(array, -1, 3))
+count = count_by_range(array, x, x)
+
+if count == 0:
+    print(-1)
+else:
+    print(count)
