a7d4712d5919c35c12883ceda9084c4ff90005b9
Kim JinHo <skulter@naver.com>
2022-04-13 13:42:29 +0900

    부녀회장이될테야

--- 파이썬_코딩테스트/test.py
+++ 파이썬_코딩테스트/test.py
@@ -1,7 +1,17 @@
-def GCD(i,j):
-    if i % j == 0:
-        return j
-    else:
-        return GCD(j, i % j)
+N = int(input())
 
-print(GCD(162,192))
+
+for i in range(N):
+    k = int(input())
+    n = int(input())
+    array = [[0] * 14 for _ in range(k+1)]
+    for i in range(14):
+        array[0][i] = i+1
+    
+    for i in range(1,k+1):
+        for j in range(n):
+            if j == 0:
+                array[i][j] = 1
+            elif array[i][j] == 0:
+                array[i][j] = array[i-1][j] + array[i][j-1]
+    print(array[k][n-1])
