ae992e7284ab65a9440684a5eec08c5467aa0a61
Kim JinHo <skulter@naver.com>
2022-04-21 13:42:26 +0900

    정렬

--- 파이썬_코딩테스트/test.py
+++ 파이썬_코딩테스트/test.py
@@ -1,17 +1,11 @@
+import sys
+
 N = int(input())
+array = []
 
+for _ in range(N):
+    array.append(int(input()))
 
+array = sorted(array)
 for i in range(N):
-    k = int(input())
-    n = int(input())
-    array = [[0] * 14 for _ in range(k+1)]
-    for i in range(14):
-        array[0][i] = i+1
-    
-    for i in range(1,k+1):
-        for j in range(n):
-            if j == 0:
-                array[i][j] = 1
-            elif array[i][j] == 0:
-                array[i][j] = array[i-1][j] + array[i][j-1]
-    print(array[k][n-1])
+    print(array[i])
