36835b11afb859e8f94e7ed1eb85a272e4dba1b6
Kim JinHo <skulter@naver.com>
2022-05-02 13:43:48 +0900

    스택수열

---
+++ 파이썬_코딩테스트/백준/스택수열_1874 (다른풀이 보기).py
@@ -0,0 +1,31 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+array = []
+target = []
+answer =[]
+isSuccess = True
+for _ in range(n):
+    target.append(int(input()))
+    
+count = 0;
+for i in target:
+    while True:
+        if len(array) > 0 and array[-1] > n:
+            isSuccess = False
+            break
+        if len(array) > 0 and i == array[-1] :
+            array.pop()
+            answer.append('-')
+            break;
+        else:
+            count += 1
+            answer.append('+')
+            array.append(count)
+if isSuccess:
+    for i in answer:
+        print(i)
+else:
+    print('NO')
