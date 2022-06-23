8259b72801f80369e627b5c39ba51fad3a8a944b
skulter <skulter@naver.com>
2022-06-24 01:52:37 +0900

    test

--- 파이썬_코딩테스트/백준/test.py
+++ 파이썬_코딩테스트/백준/test.py
@@ -1,26 +1,11 @@
-from collections import deque
+def num(m, n, x, y):
+    while x <= m * n:
+        if (x - y) % n == 0:
+            return x
+        x += m
+    return -1
 
-n,m,s = map(int,input().split())
-visited = [False] * (n+1)
-graph = [[] for i in range(n+1)]
-
-
-for _ in range(m):
-    i, j = map(int,input().split())
-    graph[i].append(j)
-    graph[j].append(i)
-    graph[i] = sorted(graph[i])
-    
-def bfs(graph,v,visited):
-    queue = deque([v])
-    visited[v]= True
-    while queue:
-        v = queue.popleft()
-        print(v, end=" ")
-        for i in graph[v]:
-            if not visited[i]:
-                visited[i] = True
-                queue.append(i)
-
-bfs(graph,s,visited)
-
+t = int(input())
+for i in range(t):
+    m, n, x, y = map(int, input().split())
+    print(num(m, n, x, y))
