28505afcea9791fe528a568c8374ea0967989fb3
skulter <skulter@naver.com>
2022-07-07 01:23:55 +0900

    테트로미노 풀고싶다..

--- 파이썬_코딩테스트/백준/풀고있는문제/테트로미노_14500.py
+++ 파이썬_코딩테스트/백준/풀고있는문제/테트로미노_14500.py
@@ -20,23 +20,26 @@ dy = [0, 0, 1, -1]
 maxValue = 0
 result = []
 
+
 def solution(x, y):
     global visited, maxValue
     maxValue = matrix[x][y]
-    value = []
+    curVal = 0
+    curMat = (0, 0)
     for _ in range(3):
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
             if 0 <= nx < N and 0 <= ny < M and not visited[nx][ny]:
-                visited[nx][ny] = True
-                value.append((matrix[nx][ny],nx,ny))
-        maxValue += max(value)
-        
-        value.clear()
-    visited = [[False] * M for _ in range(N)]
-    
+                if curVal < matrix[nx][ny]:
+                    curVal = matrix[nx][ny]
+                    curMat = (nx, ny)
+        x = curMat[0]
+        y = curMat[1]
+        maxValue += curVal
+        curVal = 0
     result.append(maxValue)
+    visited = [[False] * M for _ in range(N)]
 
 for x in range(N):
     for y in range(M):
