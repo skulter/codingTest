98fee8ba6339ec9f6be4bc913567fa630ea4df76
Kim JinHo <skulter@naver.com>
2022-07-07 13:42:53 +0900

    테트로미노 뿌시기...

--- 파이썬_코딩테스트/백준/풀고있는문제/테트로미노_14500.py
+++ 파이썬_코딩테스트/백준/풀고있는문제/테트로미노_14500.py
@@ -26,16 +26,20 @@ def solution(x, y):
     maxValue = matrix[x][y]
     curVal = 0
     curMat = (0, 0)
+    prevMat = []
     for _ in range(3):
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0 <= nx < N and 0 <= ny < M and not visited[nx][ny]:
-                if curVal < matrix[nx][ny]:
-                    curVal = matrix[nx][ny]
-                    curMat = (nx, ny)
+        for prevX, prevY in prevMat:
+            for i in range(4):
+                nx = x + dx[i]
+                ny = y + dy[i]
+                if 0 <= nx < N and 0 <= ny < M and not visited[nx][ny]:
+                    if curVal < matrix[nx][ny]:
+                        curVal = matrix[nx][ny]
+                        curMat = (nx, ny)
         x = curMat[0]
         y = curMat[1]
+        prevMat.append((x,y))
+        visited[x][y] = True
         maxValue += curVal
         curVal = 0
     result.append(maxValue)
